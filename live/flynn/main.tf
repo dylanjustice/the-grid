9eb4168096e3ea739d087fb433b1ecdfa09edd16
Dylan Justice <dylanjustice41@gmail.com>
2026-02-02 21:38:00 -0500

    Go to a single nat gateway to save some 71378$

--- live/flynn/main.tf
+++ live/flynn/main.tf
@@ -1,19 +1,6 @@
-provider "aws" {
-  region = var.region
-  default_tags {
-    tags = {
-      Project   = "the-grid"
-      Terraform = true
-      Class     = "live/flynn"
-    }
-  }
-}
-
 data "aws_availability_zones" "available" {
   region = var.region
 }
-
-
 locals {
   azs             = data.aws_availability_zones.available.names
   az_count        = length(local.azs)
@@ -21,16 +8,15 @@ locals {
   private_subnets = [for i in range(local.az_count) : cidrsubnet(var.vpc_cidr, local.subnet_newbits, i)]
   public_subnets  = [for i in range(local.az_count) : cidrsubnet(var.vpc_cidr, local.subnet_newbits, i + local.az_count)]
 }
-
 module "alpha_vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "6.6.0"
-
+  source             = "terraform-aws-modules/vpc/aws"
+  version            = "6.6.0"
   name               = "thegrid-alpha"
   cidr               = var.vpc_cidr
   azs                = local.azs
   private_subnets    = local.private_subnets
   public_subnets     = local.public_subnets
   enable_nat_gateway = true
+  single_nat_gateway = true # Cost savings
 }
 
