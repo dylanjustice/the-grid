ad9b3497b181837d73683e928dd37c045fa9e76d
Dylan Justice <dylanjustice41@gmail.com>
2026-03-18 22:43:29 -0400

    Full flow of HTTP methods

--- src/tests/http-bin.spec.ts
+++ src/tests/http-bin.spec.ts
@@ -17,7 +17,7 @@ test("HTTP Methods", async ({ page }) => {
   await deleteOp.locator("button", { hasText: "Try it out" }).click();
   await deleteOp.locator("button", { hasText: "Execute" }).click();
   await expect(
-    deleteOp.locator("td.response-col_status:not(.col_header)")
+    deleteOp.locator("td.response-col_status:not(.col_header)"),
   ).toHaveText("200");
 
   // GET /get
@@ -26,6 +26,33 @@ test("HTTP Methods", async ({ page }) => {
   await getOp.locator("button", { hasText: "Try it out" }).click();
   await getOp.locator("button", { hasText: "Execute" }).click();
   await expect(
-    getOp.locator("td.response-col_status:not(.col_header)")
+    getOp.locator("td.response-col_status:not(.col_header)"),
+  ).toHaveText("200");
+
+  // PATCH /patch
+  const patchOp = page.locator('[id="operations-HTTP Methods-patch_patch"]');
+  await patchOp.click();
+  await patchOp.locator("button", { hasText: "Try it out" }).click();
+  await patchOp.locator("button", { hasText: "Execute" }).click();
+  await expect(
+    patchOp.locator("td.response-col_status:not(.col_header)"),
+  ).toHaveText("200");
+
+  // POST /post
+  const postOp = page.locator('[id="operations-HTTP Methods-post_post"]');
+  await postOp.click();
+  await postOp.locator("button", { hasText: "Try it out" }).click();
+  await postOp.locator("button", { hasText: "Execute" }).click();
+  await expect(
+    postOp.locator("td.response-col_status:not(.col_header)"),
+  ).toHaveText("200");
+
+  // PUT /put
+  const putOp = page.locator('[id="operations-HTTP Methods-put_put"]');
+  await putOp.click();
+  await putOp.locator("button", { hasText: "Try it out" }).click();
+  await putOp.locator("button", { hasText: "Execute" }).click();
+  await expect(
+    putOp.locator("td.response-col_status:not(.col_header)"),
   ).toHaveText("200");
 });
