e5453907e28406a3885267c5b9435b9c1f252b17
Dylan Justice <dylanjustice41@gmail.com>
2026-03-19 21:54:37 -0400

    Instrument tests

--- src/tests/http-bin.spec.ts
+++ src/tests/http-bin.spec.ts
@@ -1,26 +1,48 @@
 import { test, expect } from "@playwright/test";
+import client from "prom-client";
+
+const register = new client.Registry();
+
+const histogram = new client.Histogram({
+  name: "playwright_synthetic_duration_seconds",
+  help: "Duration of Playwright synthetic test steps in seconds",
+  labelNames: ["step_name"],
+  buckets: [0.1, 0.25, 0.5, 1, 2.5, 5, 10, 30],
+  registers: [register],
+});
 
 test.beforeEach(async ({ page }) => {
   await page.goto("https://httpbin.org", { waitUntil: "networkidle" });
 });
 
+test.afterEach(async () => {
+  if (process.env.PUSHGATEWAY_URL) {
+    const gateway = new client.Pushgateway(
+      process.env.PUSHGATEWAY_URL,
+      {},
+      register,
+    );
+    await gateway.pushAdd({ jobName: "playwright-synthetics" });
+  }
+});
+
 test("HTTP Methods", async ({ page }) => {
-  // Expand the HTTP Methods section
+  const totalTimer = histogram.startTimer({ step_name: "total" });
+
   await page.locator("#operations-tag-HTTP_Methods").click();
 
-  // Wait for operations to render
+  const deleteTimer = histogram.startTimer({ step_name: "delete" });
   const deleteOp = page.locator('[id="operations-HTTP Methods-delete_delete"]');
   await deleteOp.waitFor();
-
-  // DELETE /delete
   await deleteOp.click();
   await deleteOp.locator("button", { hasText: "Try it out" }).click();
   await deleteOp.locator("button", { hasText: "Execute" }).click();
   await expect(
     deleteOp.locator("td.response-col_status:not(.col_header)"),
   ).toHaveText("200");
+  deleteTimer();
 
-  // GET /get
+  const getTimer = histogram.startTimer({ step_name: "get" });
   const getOp = page.locator('[id="operations-HTTP Methods-get_get"]');
   await getOp.click();
   await getOp.locator("button", { hasText: "Try it out" }).click();
@@ -28,8 +50,9 @@ test("HTTP Methods", async ({ page }) => {
   await expect(
     getOp.locator("td.response-col_status:not(.col_header)"),
   ).toHaveText("200");
+  getTimer();
 
-  // PATCH /patch
+  const patchTimer = histogram.startTimer({ step_name: "patch" });
   const patchOp = page.locator('[id="operations-HTTP Methods-patch_patch"]');
   await patchOp.click();
   await patchOp.locator("button", { hasText: "Try it out" }).click();
@@ -37,8 +60,9 @@ test("HTTP Methods", async ({ page }) => {
   await expect(
     patchOp.locator("td.response-col_status:not(.col_header)"),
   ).toHaveText("200");
+  patchTimer();
 
-  // POST /post
+  const postTimer = histogram.startTimer({ step_name: "post" });
   const postOp = page.locator('[id="operations-HTTP Methods-post_post"]');
   await postOp.click();
   await postOp.locator("button", { hasText: "Try it out" }).click();
@@ -46,8 +70,9 @@ test("HTTP Methods", async ({ page }) => {
   await expect(
     postOp.locator("td.response-col_status:not(.col_header)"),
   ).toHaveText("200");
+  postTimer();
 
-  // PUT /put
+  const putTimer = histogram.startTimer({ step_name: "put" });
   const putOp = page.locator('[id="operations-HTTP Methods-put_put"]');
   await putOp.click();
   await putOp.locator("button", { hasText: "Try it out" }).click();
@@ -55,4 +80,7 @@ test("HTTP Methods", async ({ page }) => {
   await expect(
     putOp.locator("td.response-col_status:not(.col_header)"),
   ).toHaveText("200");
+  putTimer();
+
+  totalTimer();
 });
