fbdad72711cd72b87d39ff08bff19fbff4ae5577
Dylan Justice <dylanjustice41@gmail.com>
2026-03-31 21:05:19 -0400

    Setting up the pushgateway

--- scripts/bootstrap-k3s.sh
+++ scripts/bootstrap-k3s.sh
@@ -3,7 +3,7 @@
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm install argocd argo/argo-cd --namespace argocd --create-namespace
-helm install prometheus prometheus-community/prometheus --namespace the-grid-monitoring --create-namespace
+helm upgrade argocd argo/argo-cd --install --namespace argocd --create-namespace
+helm upgrade prometheus prometheus-community/prometheus --install --namespace the-grid-monitoring --create-namespace --values gitops/workloads/kube-prometheus/values.yaml
 kubectl apply -f gitops/applications/argo-workflows.yaml
 kubectl apply -f gitops/applications/playwright-synthetics.yaml
